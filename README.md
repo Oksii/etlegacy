713619eb29503495128c9b8edf4323f70b85c6fc
Oksii <oxymoron.et@gmail.com>
2024-03-31 18:10:56 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,81 @@
+## Based on [msh100](https://github.com/msh100/rtcw)'s rtcwpro repository, adapted for ET Legacy and slightly simplified.
+
+Slight key differences: We build two images, a :stable tag and :latest tag
+We host our own reference files to be downloaded on build creation. By default 
+tag :latest should always feature the latest snapshot available. Builds are 
+automatically created on new snapshot releases. 
+While tag :stable is intended to be used for specific versions. 
+
+Dockerfiles are referring static files for this purpose respectively: 
+"etlegacy-latest.tar.gz" and "etlegacy-stable.tar.gz"
+
+# ET:Legacy Match Server
+
+This Docker image will download the required ET:Legacy maps as specified in the
+`MAPS` environment variable (from `REDIRECTURL`) and then spawn an ET:Legacy
+server with latest snapshot, with configuration as defined in the environment variables or
+their defaults (refer below).
+
+If you want to avoid downloading maps over HTTP(s), you can mount a volume of
+maps to `/maps/`.
+The container will first try to copy from pk3s from this directory before
+attempting an HTTP(s) download.
+
+All logs are written to STDOUT so can be viewed from `docker logs` or run
+without the `-d` Docker run switch.
+
+A container using this image will always try and download the latest changes
+from whatever `SETTINGSURL` is set to.
+By default this is [legacy-configs](https://github.com/Oksii/legacy-configs)
+
+
+## Example
+
+```
+docker run -d \
+  -p "10.0.0.1:27960:27960/udp" \
+  -e "MAPS=adlernest:te_escape2:frostbite" \
+  -e "PASSWORD=war" \
+  -e "REFEREEPASSWORD=pass123" \
+  oksii/etlegacy
+```
+
+## Configuration Options
+
+Environment Variable | Description                    | Defaults
+-------------------- | ------------------------------ | ------------------------
+MAPS                 | List of maps seperated by ':'. | Default 6 maps
+STARTMAP             | Map server starts on.          | "radar".
+REDIRECTURL          | URL of HTTP downloads          | https://index.example.com/et/
+MAP_PORT             | Container port (internal)      | 27960
+MAXCLIENTS           | Maximum number of players      | 32
+AUTO_UPDATE          | Update configurations on restart? | Enabled, set to `false` to enable.
+SETTINGSURL          | The git URL (must be HTTP public) for the ETL settings repository. | https://github.com/Oksii/legacy-configs.git
+SETTINGSBRANCH       | The git branch for the ETL settings repository. | `main`
+
+
+### Configuration parameters for the default `SETTINGSURL`
+
+Environment Variable | Description                    | Defaults
+-------------------- | ------------------------------ | ------------------------
+PASSWORD             | Server password.               | No password.
+RCONPASSWORD         | RCON password.                 | No password (disabled).
+REFEREEPASSWORD      | Referee password.              | No password (disabled).
+SCPASSWORD           | Shoutcaster password.          | No password (disabled).
+HOSTNAME             | Server hostname.               | ET
+CONF_MOTD            | MOTD line on connect           | Empty.
+TIMEOUTLIMIT         | Maximum number of pauses per map side | 1
+SERVERCONF           | The value for RtcwPro's `g_customConfig` | `legacy6`.
+
+
+Extra configuration can be prepended to the `etl_server.cfg` by mounting a
+configuration at `/legacy/server/extra.cfg`.
+This is generally not recommended, try to use the variables above where
+possible or create a custom `SETTINGSURL`.
+
+
+### Build history
+
+Version change to hash: ace61bc4a84869e3fe2a9a77d25835eac15ba4e0 snapshot legacy_v2.82.0-38-gace61bc.pk3
+
+Version change to hash: stable branch snapshot etlegacy-v2.82.0-34-gade91a7-x86_64.tar.gz
---
+++ entrypoint.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# Set config defaults
+CONF_REDIR=${REDIRECTURL:-""}
+CONF_REDIR=${REDIRECTURL:-""}
+CONF_PORT=${MAP_PORT:-27960}
+CONF_STARTMAP=${STARTMAP:-radar}
+CONF_HOSTNAME=${HOSTNAME:-ET}
+CONF_MAXCLIENTS=${MAXCLIENTS:-32}
+CONF_PASSWORD=${PASSWORD:-""}
+CONF_RCONPASSWORD=${RCONPASSWORD:-""}
+CONF_REFPASSWORD=${REFEREEPASSWORD:-""}
+CONF_SCPASSWORD=${SCPASSWORD:-""}
+CONF_TIMEOUTLIMIT=${TIMEOUTLIMIT:-1}
+CONF_SERVERCONF=${SERVERCONF:-"legacy6"}
+CONF_SETTINGSGIT=${SETTINGSURL:-"https://github.com/Oksii/legacy-configs.git"}
+CONF_SETTINGSBRANCH=${SETTINGSBRANCH:-"main"}
+
+AUTO_UPDATE=${AUTO_UPDATE:-"true"}
+
+GAME_BASE="/legacy/server"
+SETTINGS_BASE="${GAME_BASE}/settings"
+
+# Update the configs git directory
+if [ "${AUTO_UPDATE}" == "true" ]; then
+    echo "Checking if any configuration updates exist to pull"
+    if git clone --depth 1 --single-branch --branch "${CONF_SETTINGSBRANCH}" "${CONF_SETTINGSGIT}" "${SETTINGS_BASE}.new"; then
+        rm -rf "${SETTINGS_BASE}"
+        mv "${SETTINGS_BASE}.new" "${SETTINGS_BASE}"
+    else
+        echo "Configuration repo could not be pulled," \
+            "using latest pulled version"
+    fi
+fi
+
+declare -A default_maps=(
+)
+
+# Iterate over all maps and download them if necessary
+export IFS=":"
+for map in $MAPS; do
+    if [ -n "${default_maps[$map]}" ]; then
+        echo "${map} is a default map so we will not attempt to download"
+        continue
+    fi
+
+    if [ ! -f "${GAME_BASE}/etmain/${map}.pk3" ]; then
+        echo "Attempting to download ${map}"
+        if [ -f "/maps/${map}.pk3" ]; then
+            echo "Map ${map} is sourcable locally, copying into place"
+            cp "/maps/${map}.pk3" "${GAME_BASE}/etmain/${map}.pk3.tmp"
+        else
+            # TODO: We make no effort to ensure this was successful, maybe we
+            # should attempt to retry or at the very least try and skip the
+            # mutations that happen further on in the loop.
+            wget -O "${GAME_BASE}/etmain/${map}.pk3.tmp" "${CONF_REDIR}/etmain/$map.pk3"
+        fi
+
+        mv "${GAME_BASE}/etmain/${map}.pk3.tmp" "${GAME_BASE}/etmain/${map}.pk3"
+    fi
+
+    rm -rf "${GAME_BASE}/tmp/"
+done
+
+# We need to cleanup mapscripts on every invokation as we don't know what is
+# going to exist in the settings directory.
+for mapscript in "${GAME_BASE}/etmain/mapscripts/"*.script; do
+    [ -f "${mapscript}" ] || break
+    rm -rf "${mapscript}"
+done
+
+for mapscript in "${SETTINGS_BASE}/mapscripts/"*.script; do
+    [ -f "${mapscript}" ] || break
+    cp "${mapscript}" "${GAME_BASE}/etmain/mapscripts/"
+done
+
+# Copy luascripts over 
+for luascript in "${SETTINGS_BASE}/luascripts/"*.lua; do
+    [ -f "${luascript}" ] || break
+    cp "${luascript}" "${GAME_BASE}/legacy/luascripts/"
+done
+
+# Only configs live within the config directory so we don't need to be careful
+# about just recreating this directory.
+rm -rf "${GAME_BASE}/etmain/configs/"
+mkdir -p "${GAME_BASE}/etmain/configs/"
+cp "${SETTINGS_BASE}/configs/"*.config "${GAME_BASE}/etmain/configs/"
+
+# We need to set g_needpass if a password is set
+if [ "${CONF_PASSWORD}" != "" ]; then
+    CONF_NEEDPASS='set g_needpass "1"'
+fi
+
+# Iterate over all config variables and write them in place
+cp "${SETTINGS_BASE}/etl_server.cfg" "${GAME_BASE}/etmain/etl_server.cfg"
+for var in "${!CONF_@}"; do
+    value=$(echo "${!var}" | sed 's/\//\\\//g')
+    sed -i "s/%${var}%/${value}/g" "${GAME_BASE}/etmain/etl_server.cfg"
+done
+sed -i "s/%CONF_[A-Z]*%//g" "${GAME_BASE}/etmain/etl_server.cfg"
+
+# Append extra.cfg if it exists
+if [ -f "${GAME_BASE}/extra.cfg" ]; then
+    cat "${GAME_BASE}/extra.cfg" >> "${GAME_BASE}/etmain/etl_server.cfg"
+fi
+# Rtcwpro uses a different binary which is provided in their package
+binary="${GAME_BASE}/etlded"
+
+# Exec into the game
+exec "${binary}" \
+    +set sv_maxclients "${CONF_MAXCLIENTS}" \
+    +set fs_basepath "${GAME_BASE}" \
+    +set fs_homepath "/legacy/homepath" \
+    +exec "etl_server.cfg" \
+    +map "${CONF_STARTMAP}" \
+    "${@}"
